2d7132936b106a84b9a26cd9a6949c442cf8e390
Yan Braslavsky <yan.braslavsky@gmail.com>
2024-07-27 10:41:33 +0200

    chore: Update firebaseConfig.js with environment variables for Firebase configuration

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,24 +3,14 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  // Your Firebase configuration object
-  // You'll need to replace this with your actual Firebase config
-//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-//   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
-
-  apiKey: "AIzaSyBR-Gz9XGQj-lp_yx4xcqMxsUVWQbeV-e4",
-  authDomain: "fourtyfit-44a5b.firebaseapp.com",
-  projectId: "fourtyfit-44a5b",
-  storageBucket: "fourtyfit-44a5b.appspot.com",
-  messagingSenderId: "143026831684",
-  appId: "1:143026831684:web:841f10900b0b96255d9711",
-  measurementId: "G-S7VW2DCQ1J"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
-const app = initializeApp(firebaseConfig,'EXPO_APP');
+const app = initializeApp(firebaseConfig, 'EXPO_APP');
 export const db = getFirestore(app);
